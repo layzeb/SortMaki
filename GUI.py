359bfdc05a0055aa8879e718a3708ae904fb8339
Layze Brandão <layzebrand@gmail.com>
2017-09-16 15:23:27 +0000

    Lógica do gerador de random e métodos de eventos

--- GUI.py
+++ GUI.py
@@ -1,18 +1,18 @@
 from tkinter import *
+from random import randrange, seed
+
 
 class Sorteador:
+
     def __init__(self, janela):   #colocar todos os widgets dentro da classe
 
         # criando as fontes que serão usadas no programa
         # Fonte é definida como uma tupla contendo nome/tam/estilo
-        self.tfont = ('Verdana','20','bold')    #fonte do título
         self.cfont = ('Verdana', '12', 'bold')      #fonte do corpo
-        self.bfont = ('Verdana', '14', 'bold')
+        self.bfont = ('Verdana', '14', 'bold')      #fonte do botao
 
 
-        # criando imagens
-        logo = PhotoImage(file = 'makilandialogo.gif')
-        sorteio = PhotoImage(file = 'sorteio3.gif')
+        #----------------------------------------------------------------------------------------------------
 
 
         # criando Frames (organizador de widgets dentro do conteiner)
@@ -38,6 +38,13 @@ class Sorteador:
         self.frame5.pack()
         self.frame4.pack()
 
+        #-------------------------------------------------------------------------------------------------------
+
+
+        # criando imagens
+        logo = PhotoImage(file = 'makilandialogo.gif')
+        sorteio = PhotoImage(file = 'sorteio3.gif')
+
         # imagem do sorteio
         self.sorteio = Label(self.frame1)
         self.sorteio['image'] = sorteio
@@ -50,7 +57,7 @@ class Sorteador:
         self.logo.image = logo
         self.logo.pack()
 
-
+        #------------------------------------------------------------------------------------------------------
 
         # cria o texto na tela (Label)
         # O metodo Label deve chamar o pai (o conteiner/instancia em que está contido, no caso, a janela e o texto
@@ -59,35 +66,103 @@ class Sorteador:
         #self.titulo.pack()
 
         self.lb_numeros = Label(self.frame2, text='Quantos números deseja sortear? ', font = self.cfont)
+
         self.ent_numeros = Entry(self.frame2, width = 8)		# cria uma entrada de texto
+        self.ent_numeros.focus_force()      #faz o programa iniciar com a primeira entrada focada com cursor
+
         self.lb_range = Label(self.frame3, text='Sortear do número 1 ao: ', font = self.cfont)
         self.ent_range = Entry(self.frame3, width = 8)
+        self.ent_range.bind('<Return>', self.go_enter)
 
         self.lb_numeros.pack(side = LEFT)
-        self.ent_numeros.pack(side = RIGHT)
+        self.ent_numeros.pack(side = LEFT)
         self.lb_range.pack(side = LEFT)
-        self.ent_range.pack(side = RIGHT)
+        self.ent_range.pack(side = LEFT)
 
         # cria o check button
         self.lb_ord = Label(self.frame5, text = 'Ordenar resultado? ', font = self.cfont)
         self.ordenar = Checkbutton(self.frame5, text = 'Sim', font = self.cfont)
         self.lb_ord.pack(side = LEFT)
-        self.ordenar.pack(side = RIGHT)
+        self.ordenar.pack(side = LEFT)
 
 
         # cria um botao
         # command lida com o evento, acionando o método criado
-        self.botao = Button(self.frame4, text = 'Sortear', bg = '#25b05d', command = self.gerador, width = 10, font = self.bfont)
+        self.botao = Button(self.frame4, text = 'Sortear', bg = '#25b05d', command = self.checker, width = 10, font = self.bfont)
         self.botao.pack()
 
         # exibe os numeros sorteados
         self.lb_resultado = Label(self.frame4, font = self.cfont, pady = 8)
         self.lb_resultado.pack()
 
-    def gerador(self):
-        """Método para criar o texto quando o botão for acionado"""
-        self.lb_resultado['text'] = 'Números sorteados: '
-        self.lb_resultado['fg'] = 'blue'
+
+    def checker(self):
+        '''Testa as entradas antes de calcular o resultado'''
+        ent1 = self.ent_numeros.get()
+        ent2 = self.ent_range.get()
+        if ent1 == '' or ent2 == '':
+            self.missing_entry()
+        elif not ent1.isnumeric() or not ent2.isnumeric():
+            self.erro_alfa()
+        else:
+            self.aleatorio()
+
+
+
+    def go_enter(self, event):
+        """Determina a ativacao do botao sortear caso a segunda entrada esteja focada"""
+        self.checker()
+
+    def erro_alfa(self):
+        self.lb_resultado['text'] = 'Digite somente números inteiros'
+        self.lb_resultado['fg'] = 'red'
+        self.ent_numeros.focus_force()
+
+
+    def erro_valor(self):
+        self.lb_resultado['text'] = 'Valor inválido'
+        self.lb_resultado['fg'] = 'red'
+        self.ent_numeros.focus_force()
+
+
+    def missing_entry(self):
+        self.lb_resultado['text'] = 'Campos obrigatórios'
+        self.lb_resultado['fg'] = 'red'
+        self.ent_numeros.focus_force()
+
+
+    # -------------------------------------------------------------------------------------------------------
+    # LOGICA
+
+    def aleatorio(self):
+
+        seed()
+
+        qt_num = int(self.ent_numeros.get())
+        stop = int(self.ent_range.get())
+
+        if qt_num < stop:
+
+            if qt_num == 1:
+                sorteado = randrange(1, stop)
+                self.lb_resultado['text'] = ('Número sorteado: {}'.format(sorteado))
+                self.lb_resultado['fg'] = 'blue'
+            elif qt_num > 1:
+                aux = []
+                ganhadores = []
+                while len(ganhadores) < qt_num:
+                    aux.append(randrange(1, stop))
+                    for numero in aux:
+                        if numero not in ganhadores:
+                            ganhadores.append(numero)
+                sorteado = ganhadores
+
+                self.lb_resultado['fg'] = 'blue'
+                self.lb_resultado['text'] = ('Números sorteados: {}'.format(sorteado))
+            else:
+                self.erro_valor()
+        else:
+            self.erro_valor()
 
 
 
